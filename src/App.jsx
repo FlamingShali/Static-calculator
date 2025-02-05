f8e320b0fe55f3e2b5d7690c9c0cb2615505be06
Michał Klepner <michal.klepner1@o2.pl>
2025-02-05 04:49:18 +0100

    Added styled calc numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styled from "styled-components";
 
-function App() {
-  const [count, setCount] = useState(0)
+const calcArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [0]];
+const mathOperations = ["-", "+", "/", "*"];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Calculator = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Row = styled.ul`
+  display: flex;
+  flex-direction: row;
+  margin: 0;
+`;
+const CalcButton = styled.button`
+  width: 100px;
+  height: 100px;
+`;
+
+function calc(event) {
+  console.log(event.target.value);
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <Calculator>
+        {calcArray.map((row) => (
+          <Row key={row}>
+            {row.map((number) => (
+              <CalcButton onClick={calc} value={number} key={number}>
+                {number}
+              </CalcButton>
+            ))}
+          </Row>
+        ))}
+      </Calculator>
+      <p>cos</p>
+    </>
+  );
+}
+
+export default App;
